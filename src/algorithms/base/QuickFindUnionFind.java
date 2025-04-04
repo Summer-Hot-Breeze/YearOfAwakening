bf62ca2f39ce456e7e310a14bf1e84a8e5b2aaf2
zubing <ouzubing_xrw@126.com>
2025-04-04 08:46:04 +0800

    基于《算法》第四版第一章1.5节实现quick find算法

---
+++ src/algorithms/base/QuickFindUnionFind.java
@@ -0,0 +1,42 @@
+package algorithms.base;
+
+public class QuickFindUnionFind {
+    //这里是将什么进行了抽象？触点、连通分量、连接
+    private int count;
+    private int[] id;
+
+    public QuickFindUnionFind(int count) {
+        this.count = count;
+        id = new int[count];
+        for (int i = 0; i < count; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connect(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        return id[p];
+    }
+
+    public void union(int p, int q) {
+        int pId=id[p];
+        int qId=id[q];
+        if(pId==qId){
+            return;
+        }
+        for (int i = 0; i < id.length; i++) {
+            if(id[i]==pId){
+                id[i]=qId;
+            }
+        }
+        //这里的count为什么要放在外面进行--
+        count--;
+    }
+}
